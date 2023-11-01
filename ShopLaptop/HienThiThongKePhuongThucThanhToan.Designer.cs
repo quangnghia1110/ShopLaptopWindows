e78230dd95ca17eda3e79b857a8252fdf5c823f7
Vu Le Van <levanvu220603@gmail.com>
2023-11-01 21:25:25 +0700

    vu test git

--- ShopLaptop/HienThiThongKePhuongThucThanhToan.Designer.cs
+++ ShopLaptop/HienThiThongKePhuongThucThanhToan.Designer.cs
@@ -48,19 +48,21 @@
             this.tabControl1.Controls.Add(this.tabPage5);
             this.tabControl1.Controls.Add(this.tabPage6);
             this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabControl1.Location = new System.Drawing.Point(3, 78);
+            this.tabControl1.Location = new System.Drawing.Point(3, 98);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(936, 601);
+            this.tabControl1.Size = new System.Drawing.Size(1053, 752);
             this.tabControl1.TabIndex = 22;
             // 
             // tabPage5
             // 
             this.tabPage5.Controls.Add(this.dataGridView5);
             this.tabPage5.Location = new System.Drawing.Point(4, 4);
+            this.tabPage5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(928, 568);
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage5.Size = new System.Drawing.Size(1045, 714);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Tiền mặt";
             this.tabPage5.UseVisualStyleBackColor = true;
@@ -68,20 +70,22 @@
             // dataGridView5
             // 
             this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView5.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView5.Location = new System.Drawing.Point(7, 8);
+            this.dataGridView5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView5.Name = "dataGridView5";
             this.dataGridView5.RowHeadersWidth = 51;
             this.dataGridView5.RowTemplate.Height = 24;
-            this.dataGridView5.Size = new System.Drawing.Size(916, 574);
+            this.dataGridView5.Size = new System.Drawing.Size(1030, 718);
             this.dataGridView5.TabIndex = 2;
             // 
             // tabPage6
             // 
             this.tabPage6.Controls.Add(this.dataGridView6);
             this.tabPage6.Location = new System.Drawing.Point(4, 4);
+            this.tabPage6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage6.Size = new System.Drawing.Size(928, 582);
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage6.Size = new System.Drawing.Size(1045, 713);
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "Chuyển khoản";
             this.tabPage6.UseVisualStyleBackColor = true;
@@ -89,20 +93,21 @@
             // dataGridView6
             // 
             this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView6.Location = new System.Drawing.Point(3, 5);
+            this.dataGridView6.Location = new System.Drawing.Point(3, 6);
+            this.dataGridView6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView6.Name = "dataGridView6";
             this.dataGridView6.RowHeadersWidth = 51;
             this.dataGridView6.RowTemplate.Height = 24;
-            this.dataGridView6.Size = new System.Drawing.Size(916, 574);
+            this.dataGridView6.Size = new System.Drawing.Size(1030, 718);
             this.dataGridView6.TabIndex = 2;
             // 
             // gunaLabel3
             // 
             this.gunaLabel3.AutoSize = true;
             this.gunaLabel3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gunaLabel3.Location = new System.Drawing.Point(597, 31);
+            this.gunaLabel3.Location = new System.Drawing.Point(672, 39);
             this.gunaLabel3.Name = "gunaLabel3";
-            this.gunaLabel3.Size = new System.Drawing.Size(122, 19);
+            this.gunaLabel3.Size = new System.Drawing.Size(149, 24);
             this.gunaLabel3.TabIndex = 21;
             this.gunaLabel3.Text = "Thống kê theo: ";
             // 
@@ -117,24 +122,25 @@
             this.gunaComboBox3.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.gunaComboBox3.ForeColor = System.Drawing.Color.Black;
             this.gunaComboBox3.FormattingEnabled = true;
-            this.gunaComboBox3.Location = new System.Drawing.Point(763, 24);
+            this.gunaComboBox3.Location = new System.Drawing.Point(858, 30);
             this.gunaComboBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gunaComboBox3.Name = "gunaComboBox3";
             this.gunaComboBox3.OnHoverItemBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
             this.gunaComboBox3.OnHoverItemForeColor = System.Drawing.Color.White;
-            this.gunaComboBox3.Size = new System.Drawing.Size(175, 31);
+            this.gunaComboBox3.Size = new System.Drawing.Size(196, 35);
             this.gunaComboBox3.TabIndex = 20;
             // 
             // HienThiThongKePhuongThucThanhToan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(943, 810);
+            this.ClientSize = new System.Drawing.Size(1061, 1012);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.gunaLabel3);
             this.Controls.Add(this.gunaComboBox3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "HienThiThongKePhuongThucThanhToan";
             this.Text = "HienThiThongKePhuongThucThanhToan";
             this.tabControl1.ResumeLayout(false);
