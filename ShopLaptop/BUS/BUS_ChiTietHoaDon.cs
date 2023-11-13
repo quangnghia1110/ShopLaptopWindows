685957b84812096f0c1cefb8798f0af661ea94a5
n0aHVN <superherodung123@gmail.com>
2023-11-13 19:01:25 +0700

    13/11

--- ShopLaptop/BUS/BUS_ChiTietHoaDon.cs
+++ ShopLaptop/BUS/BUS_ChiTietHoaDon.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using ShopLaptop.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ShopLaptop.BUS
 {
-    internal class BUS_ChiTietHoaDon
+    public class BUS_ChiTietHoaDon
     {
+        public DataTable LoadChiTietHoaDons()
+        {
+            DAL_ChiTietHoaDon dal = new DAL_ChiTietHoaDon();
+            return dal.LoadChiTietHoaDons();
+        }
     }
 }
--- ShopLaptop/DAL/DAL_ChiTietHoaDon.cs
+++ ShopLaptop/DAL/DAL_ChiTietHoaDon.cs
@@ -13,99 +13,92 @@ namespace ShopLaptop.DAL
     public class DAL_ChiTietHoaDon
     {
         MyConnect myConnect = new MyConnect();
+        ShopLaptopDBDataContext db = new ShopLaptopDBDataContext();
         public DataTable LoadChiTietHoaDons()
         {
-            try
+            DataTable dataTable = new DataTable();
+            try 
             {
-                this.myConnect.openConnection();
-                SqlCommand cmd = new SqlCommand("SELECT * FROM ChiTietHoaDon",
-                    myConnect.getConnection);
-
                 DataTable dt = new DataTable();
-                myConnect.openConnection();
-                SqlDataReader dr = cmd.ExecuteReader();
-                myConnect.closeConnection();
-                return dt;
+                var list = (from hoadon in db.ChiTietHoaDons select hoadon).ToList();
+                dataTable = CustomFuncs.ConvertListToDataTable(list);
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error:" + ex.Message, "Lỗi", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                return null;
             }
+            return dataTable;
         }
 
         public bool AddChiTietHoaDon(string maLT, string maHD, string soLuongSP, string giaBanTungSP, string thueVAT, string khuyenMai, string thanhTienTungSP)
         {
-            bool isInsertSuccessfully = false;
+            bool isSuccess = false;
             try
             {
-                myConnect.openConnection();
-                SqlCommand cmd = new SqlCommand(
-                    $"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Insert' ",
-                    myConnect.getConnection);
-                cmd.ExecuteNonQuery();
-                isInsertSuccessfully = true;
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show("Error:" + ex.Message, "Lỗi", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                isInsertSuccessfully = false;
+                //myConnect.openConnection();
+                //SqlCommand cmd = new SqlCommand(
+                //    $"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Insert' ",
+                //    myConnect.getConnection);
+                //cmd.ExecuteNonQuery();
+                //isSuccess = true;
+                int numberOfModifiedRow = db.ExecuteCommand($"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Insert' ");
+                db.SubmitChanges();
+                isSuccess = numberOfModifiedRow > 0;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error:" + ex.Message, "Lỗi", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                isInsertSuccessfully = false;
+                isSuccess = false;
             }
-            finally { myConnect.closeConnection(); }
-            return isInsertSuccessfully;
+            return isSuccess;
         }
 
         public bool EditChiTietHoaDon(string maLT, string maHD, string soLuongSP, string giaBanTungSP, string thueVAT, string khuyenMai, string thanhTienTungSP)
         {
-            bool isEditSuccessfully = false;
+            bool isSuccess = false;
             try
             {
-                myConnect.openConnection();
-                SqlCommand cmd = new SqlCommand($"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Update'", myConnect.getConnection);
-                cmd.ExecuteNonQuery();
-                isEditSuccessfully = true;
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show("Error:" + ex.Message, "Lỗi", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                isEditSuccessfully = false;
+                //myConnect.openConnection();
+                //SqlCommand cmd = new SqlCommand($"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Update'", myConnect.getConnection);
+                //cmd.ExecuteNonQuery();
+                //isEditSuccessfully = true;
+                int numberOfModifiedRow = db.ExecuteCommand($"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Update' ");
+                db.SubmitChanges();
+                isSuccess = numberOfModifiedRow > 0;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error:" + ex.Message, "Lỗi", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                isEditSuccessfully = false;
+                isSuccess = false;
             }
-            finally { myConnect.closeConnection(); }
-            return isEditSuccessfully;
+            return isSuccess;
         }
 
         public bool XoaChiTietHoaDon(string maLT, string maHD, string soLuongSP, string giaBanTungSP, string thueVAT, string khuyenMai, string thanhTienTungSP)
         {
-            bool isDeleteSuccessfully = false;
+            bool isSuccess = false;
             try
             {
-                myConnect.openConnection();
-                SqlCommand cmd = new SqlCommand($"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Delete'", myConnect.getConnection);
-                cmd.ExecuteNonQuery();
-                isDeleteSuccessfully = true;
+                //myConnect.openConnection();
+                //SqlCommand cmd = new SqlCommand($"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Delete'", myConnect.getConnection);
+                //cmd.ExecuteNonQuery();
+                //isSuccess = true;
+                int numberOfModifiedRow = db.ExecuteCommand($"EXEC sp_ReviseChiTietHoaDon '{maLT}', '{maHD}', '{soLuongSP}', {giaBanTungSP}, '{thueVAT}', '{khuyenMai}', '{thanhTienTungSP}', 'Delete' ");
+                db.SubmitChanges();
+                isSuccess = numberOfModifiedRow > 0;
             }
             catch (SqlException ex)
             {
                 MessageBox.Show("Error:" + ex.Message, "Lỗi", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                isDeleteSuccessfully =  false;
+                isSuccess =  false;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error:" + ex.Message, "Lỗi", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                isDeleteSuccessfully = false;
+                isSuccess = false;
             }
             finally { myConnect.closeConnection(); }
-            return isDeleteSuccessfully;
+            return isSuccess;
         }
     }
 }
